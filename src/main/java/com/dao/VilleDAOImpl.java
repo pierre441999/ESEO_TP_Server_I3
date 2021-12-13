8a96289f772101544a9bf33fd093911dfcfa1c43
Pierre <pierrebusson44@hotmail.fr>
2021-12-13 16:20:10 +0100

    Update VilleDAOImpl.java
    close connection

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -18,7 +18,6 @@ public class VilleDAOImpl implements VilleDAO {
 	private Logger logger = LoggerFactory.getLogger(VilleDAOImpl.class);
 	private BddConfig bddconfig;
 
-
 	public VilleDAOImpl(BddConfig bddConfig) {
 		this.bddconfig = bddConfig;
 	}
@@ -26,68 +25,31 @@ public class VilleDAOImpl implements VilleDAO {
 	@Override
 	public ArrayList<Ville> findAllVilles() {
 		ArrayList<Ville> listVille = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultat = null;
+		try {
+			connection = bddconfig.getConnection();
+			preparedStatement = connection.prepareStatement("SELECT * FROM ville_france ;");
+			resultat = preparedStatement.executeQuery();
+			while (resultat.next()) {
+				String code_Commune = resultat.getString("Code_Commune_INSEE");
+				String nomCommune = resultat.getString("Nom_Commune");
+				String codePostal = resultat.getString("Code_postal");
+				String libelle = resultat.getString("Libelle_acheminement");
+				String ligne_5 = resultat.getString("Ligne_5");
+				String latitude = resultat.getString("Latitude");
+				String longitude = resultat.getString("Longitude");
+				Coordonnees coor = new Coordonnees(latitude, longitude);
 
-		try (Connection connexion = bddconfig.getConnection();
-				PreparedStatement preparedStatement = connexion
-						.prepareStatement("SELECT * FROM ville_france ;")) {
+				Ville ville = new Ville(code_Commune, nomCommune, codePostal, libelle, ligne_5, coor);
+				listVille.add(ville);
 
-			try (ResultSet resultat = preparedStatement.executeQuery()) {
-				while (resultat.next()) {
-					String code_Commune = resultat.getString("Code_Commune_INSEE");
-					String nomCommune = resultat.getString("Nom_Commune");
-					String codePostal = resultat.getString("Code_postal");
-					String libelle = resultat.getString("Libelle_acheminement");
-					String ligne_5 = resultat.getString("Ligne_5");
-					String latitude = resultat.getString("Latitude");
-					String longitude = resultat.getString("Longitude");
-					Coordonnees coor = new Coordonnees(latitude, longitude);
-					
-			
-					Ville ville = new Ville(code_Commune, nomCommune, codePostal,
-							libelle, ligne_5, coor);
-					listVille.add(ville);
-					
-				}
-				
-				
 			}
-
-		} catch (SQLException e) {
-			this.logger.error("Impossible d'exécuter la requête.", e);
+			preparedStatement.close();
+			resultat.close();
 		}
-		
-		
 
-		return listVille;
-	}
-	
-	public ArrayList<Ville> findSpecificVille(String codeCommune) {
-		ArrayList<Ville> listVille = new ArrayList<Ville>();
-
-		try (Connection connexion = bddconfig.getConnection();
-				PreparedStatement preparedStatement = connexion
-						.prepareStatement("SELECT * FROM ville_france WHERE Code_Commune_INSEE=?")) {
-			preparedStatement.setString(1, codeCommune);
-			try (ResultSet resultat = preparedStatement.executeQuery()) {
-				while (resultat.next()) {
-					String code_Commune = resultat.getString("Code_Commune_INSEE");
-					String nomCommune = resultat.getString("Nom_Commune");
-					String codePostal = resultat.getString("Code_postal");
-					String libelle = resultat.getString("Libelle_acheminement");
-					String ligne_5 = resultat.getString("Ligne_5");
-					String latitude = resultat.getString("Latitude");
-					String longitude = resultat.getString("Longitude");
-					Coordonnees coor = new Coordonnees(latitude, longitude);
-					
-			
-					Ville ville = new Ville(code_Commune, nomCommune, codePostal,
-							libelle, ligne_5, coor);
-					listVille.add(ville);
-				}
-			}
-
-		} 
-		
 		catch (SQLException e) {
 			this.logger.error("Impossible d'exécuter la requête.", e);
 		}
@@ -95,14 +57,48 @@ public class VilleDAOImpl implements VilleDAO {
 		return listVille;
 	}
 
-	public void addVille(Ville ville){
-		try (Connection connexion = bddconfig.getConnection();
-				PreparedStatement preparedStatement = connexion
-						.prepareStatement("INSERT INTO ville_france "
-								+ "(Code_Commune_INSEE, Nom_Commune, Code_postal,"
-								+ "Libelle_acheminement, Ligne_5, Latitude, Longitude)"
-								+ "VALUES (?, ?, ?, ?, ?, ?, ?);"))
-		{
+	public ArrayList<Ville> findSpecificVille(String codeCommune) {
+		ArrayList<Ville> listVille = new ArrayList<Ville>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultat = null;
+		try {
+			connection = bddconfig.getConnection();
+			preparedStatement = connection.prepareStatement("SELECT * FROM ville_france WHERE Code_Commune_INSEE=?");
+			preparedStatement.setString(1, codeCommune);
+			resultat = preparedStatement.executeQuery();
+			while (resultat.next()) {
+				String code_Commune = resultat.getString("Code_Commune_INSEE");
+				String nomCommune = resultat.getString("Nom_Commune");
+				String codePostal = resultat.getString("Code_postal");
+				String libelle = resultat.getString("Libelle_acheminement");
+				String ligne_5 = resultat.getString("Ligne_5");
+				String latitude = resultat.getString("Latitude");
+				String longitude = resultat.getString("Longitude");
+				Coordonnees coor = new Coordonnees(latitude, longitude);
+
+				Ville ville = new Ville(code_Commune, nomCommune, codePostal, libelle, ligne_5, coor);
+				listVille.add(ville);
+			}
+
+			preparedStatement.close();
+			resultat.close();
+		} catch (SQLException e) {
+			this.logger.error("Impossible d'exécuter la requête.", e);
+		}
+
+		return listVille;
+	}
+
+	public void addVille(Ville ville) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = bddconfig.getConnection();
+			preparedStatement = connection
+					.prepareStatement("INSERT INTO ville_france " + "(Code_Commune_INSEE, Nom_Commune, Code_postal,"
+							+ "Libelle_acheminement, Ligne_5, Latitude, Longitude)" + "VALUES (?, ?, ?, ?, ?, ?, ?);");
+
 			preparedStatement.setString(1, ville.getCodeCommune());
 			preparedStatement.setString(2, ville.getNomCommune());
 			preparedStatement.setString(3, ville.getCodePostale());
@@ -111,26 +107,23 @@ public class VilleDAOImpl implements VilleDAO {
 			preparedStatement.setString(6, ville.getCoord().getLatitude());
 			preparedStatement.setString(7, ville.getCoord().getLongitude());
 			preparedStatement.executeUpdate();
-			connexion.commit();
-			
-			
-		} 
-		
-		catch (SQLException e) {
+			connection.commit();
+			preparedStatement.close();
+		} catch (SQLException e) {
 			this.logger.error("Impossible d'exécuter la requête.", e);
 		}
-		
-		
+
 	}
-	
-	public void updateVille(Ville ville, String codeCommune){
-		try (Connection connexion = bddconfig.getConnection();
-				PreparedStatement preparedStatement = connexion
-						.prepareStatement("UPDATE ville_france SET "
-								+ "Code_Commune_INSEE = ?, Nom_Commune = ?, Code_postal = ?,"
-								+ "Libelle_acheminement = ?, Ligne_5 = ?, Latitude = ?,"
-								+ "Longitude = ?"
-								+ "where Code_Commune_INSEE = ?;")){     
+
+	public void updateVille(Ville ville, String codeCommune) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = bddconfig.getConnection();
+			preparedStatement = connection.prepareStatement(
+					"UPDATE ville_france SET " + "Code_Commune_INSEE = ?, Nom_Commune = ?, Code_postal = ?,"
+							+ "Libelle_acheminement = ?, Ligne_5 = ?, Latitude = ?," + "Longitude = ?"
+							+ "where Code_Commune_INSEE = ?;");
 			preparedStatement.setString(1, ville.getCodeCommune());
 			preparedStatement.setString(2, ville.getNomCommune());
 			preparedStatement.setString(3, ville.getCodePostale());
@@ -140,28 +133,27 @@ public class VilleDAOImpl implements VilleDAO {
 			preparedStatement.setString(7, ville.getCoord().getLongitude());
 			preparedStatement.setString(8, codeCommune);
 			preparedStatement.executeUpdate();
-			connexion.commit();
+			connection.commit();
+			preparedStatement.close();
 		} catch (SQLException e) {
 			this.logger.error("Impossible d'exécuter la requête.", e);
 		}
 	}
-	
+
 	public void removeVille(String codeCommune) {
-		
-		try (Connection connexion = bddconfig.getConnection();
-				PreparedStatement preparedStatement = connexion
-						.prepareStatement("DELETE FROM ville_france where Code_Commune_INSEE = ?;")){     
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = bddconfig.getConnection();
+			preparedStatement = connection.prepareStatement("DELETE FROM ville_france where Code_Commune_INSEE = ?;");
 			preparedStatement.setString(1, codeCommune);
 			preparedStatement.executeUpdate();
-			connexion.commit();
+			connection.commit();
+			preparedStatement.close();
 		} catch (SQLException e) {
 			this.logger.error("Impossible d'exécuter la requête.", e);
 		}
-		
+
 	}
-
-
-
-
 
 }
